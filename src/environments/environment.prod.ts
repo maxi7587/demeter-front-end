1da262fb06372879b1c349a2b6d12fb0f6e883f0
Maxi <maxi7587@hotmail.com>
2019-04-23 21:16:30 -0300

    filled environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const mpurl = '//127.0.0.1:8000/';
+// const mpurl = '//192.168.0.12:8000/';
+
 export const environment = {
-  production: true
+    production: true,
+    env: 'production',
+    MPURL: mpurl,
+    APIURL: mpurl,
+    whiteListedDomains: ['127.0.0.1:8000', '127.0.0.1', '192.168.0.12:8000']
 };
