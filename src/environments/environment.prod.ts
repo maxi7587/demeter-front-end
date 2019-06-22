80a5724323ce13dd7f6a2cdb806b08ba0d048149
Maxi <maxi7587@hotmail.com>
2019-06-22 00:18:31 -0300

    updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,8 @@ export const environment = {
     env: 'production',
     MPURL: mpurl,
     APIURL: mpurl,
-    clientId: 'T9BRYwu2j6oqlkF0GhbT7ueuqLRrDV3ey8cPVXhS',
+    clientId: 'IUPfaJdxUwkHfSbiXSUyzmIypqC1MDn3XCJeVMUk',
     clientSecret:
-        `iPULFnl5bU5BPyPpeSGPi2bjCCM8msiAb192jjlRupiGyEcDPwYKR6bYlI04QCxClJyyNHZhXRnpo2omLrQ9PkcIIHYcRTJy6dFUDU46qg5UDUX2otaZ8W0Z8l1kmmP6`,
+        `5IkrTIn0OpGOnHwhWLfUN6mNWZqHV02mjs1iQuOJNFGolJ23NK8ZpUZFUi51KJ1Dn1h81njmshoKdVmzpVzo6BlRQXEyIOYZmQCkwvR9InSjZccBGSl18onvlHEdVV3H`,
     whiteListedDomains: ['demeter.pythonanywhere.com', '127.0.0.1:8000', '127.0.0.1', '192.168.0.12:8000']
 };
