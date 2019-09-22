4e592f1fb3771e6e699c7fd982b6fe37ea228c91
Maxi <maxi7587@hotmail.com>
2019-09-22 04:18:20 -0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,11 @@ export const environment = {
     clientId: 'IUPfaJdxUwkHfSbiXSUyzmIypqC1MDn3XCJeVMUk',
     clientSecret:
         `5IkrTIn0OpGOnHwhWLfUN6mNWZqHV02mjs1iQuOJNFGolJ23NK8ZpUZFUi51KJ1Dn1h81njmshoKdVmzpVzo6BlRQXEyIOYZmQCkwvR9InSjZccBGSl18onvlHEdVV3H`,
-    whiteListedDomains: ['demeter.pythonanywhere.com', '127.0.0.1:8000', '127.0.0.1', '192.168.0.12:8000']
+    whiteListedDomains: [
+        'demeter.pythonanywhere.com',
+        'tranquil-sands-20400.herokuapp.com',
+        '127.0.0.1:8000',
+        '127.0.0.1',
+        '192.168.0.12:8000'
+    ]
 };
